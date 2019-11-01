c42dd8f1f852460ca2744bdd3e2014c65ef32ddc
Shingo-Mochinushi <56183544+Shingo-Mochinushi@users.noreply.github.com>
2019-11-01 19:37:59 +0900

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -227,26 +227,26 @@ function liffGetUserService(service) {
         uiStatusError(makeErrorMsg(error), false);
     });
 
-    service.getCharacteristic(CONFIG_CHARACTERISTIC_UUID).then(characteristic => {
-        return characteristic.readValue();
-    }).then(value => {
-        const device_nick = new Uint8Array(value.buffer)
-            .reduce((output,byte) => output + String.fromCharCode(byte),"");
-        document.getElementById("device-nick").innerText = device_nick;
-    }).catch(error => {
-        uiStatusError(makeErrorMsg(error), false);
-    });
-
-//    service.getCharacteristic(CONFIG2_CHARACTERISTIC_UUID).then(characteristic => {
+//    service.getCharacteristic(CONFIG_CHARACTERISTIC_UUID).then(characteristic => {
 //        return characteristic.readValue();
 //    }).then(value => {
-//        const device_uuid = new Uint8Array(value.buffer)
+//        const device_nick = new Uint8Array(value.buffer)
 //            .reduce((output,byte) => output + String.fromCharCode(byte),"");
-//        document.getElementById("device-uuid").innerText = device_uuid;
+//        document.getElementById("device-nick").innerText = device_nick;
 //    }).catch(error => {
-//        alert(error.message);
 //        uiStatusError(makeErrorMsg(error), false);
 //    });
+
+    service.getCharacteristic(CONFIG2_CHARACTERISTIC_UUID).then(characteristic => {
+        return characteristic.readValue();
+    }).then(value => {
+        const device_uuid = new Uint8Array(value.buffer)
+            .reduce((output,byte) => output + String.fromCharCode(byte),"");
+        document.getElementById("device-uuid").innerText = device_uuid;
+    }).catch(error => {
+        alert(error.message);
+        uiStatusError(makeErrorMsg(error), false);
+    });
 }
 
     function liffGetPSDIService(service) {
