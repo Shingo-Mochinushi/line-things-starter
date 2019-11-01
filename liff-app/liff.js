506d1880065b321e43a3909288b04671ae8365d6
Shingo-Mochinushi <56183544+Shingo-Mochinushi@users.noreply.github.com>
2019-11-01 18:34:10 +0900

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -230,8 +230,8 @@ function liffGetUserService(service) {
     service.getCharacteristic(CONFIG_CHARACTERISTIC_UUID).then(characteristic => {
         return characteristic.readValue();
     }).then(value => {
-        device_nick = new Uint8Array(value.buffer).
-            reduce((output,byte) => output + byte.toString(),"");
+        device_nick = new Uint8Array(value.buffer)
+            .reduce((output,byte) => output + String.fromCharCode(byte),"");
         document.getElementById("device-nick").innerText = device_nick;
     }).catch(error => {
         uiStatusError(makeErrorMsg(error), false);
